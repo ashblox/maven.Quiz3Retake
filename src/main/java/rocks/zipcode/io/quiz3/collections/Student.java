39ba3cd12f2155558c9cfc08d1b5b4577c1d5c4c
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-03-25 11:36:10 -0400

    finished collections

--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -7,24 +9,50 @@ import java.util.List;
  */
 public class Student {
 
+    private List<Lab> labs;
+
     public Student() {
-        this(null);
+        this(new ArrayList<>());
     }
 
     public Student(List<Lab> labs) {
+        this.labs = labs;
     }
 
     public Lab getLab(String labName) {
-        return null;
+        Lab retrieved = null;
+        for (Lab lab : labs) {
+            if (lab.getName().equals(labName)) {
+                retrieved = lab;
+            }
+        }
+        return retrieved;
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        Lab toSet = getLab(labName);
+        if (toSet == null) {
+            throw new UnsupportedOperationException("Lab has not been forked");
+        } else {
+            toSet.setStatus(labStatus);
+        }
     }
 
     public void forkLab(Lab lab) {
+        labs.add(lab);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return getLab(labName).getStatus();
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        labs.sort(Comparator.comparing(Lab::getName));
+        for (Lab lab : labs) {
+            str += String.format("%s > %s\n", lab.getName(), lab.getStatus());
+        }
+        return str.trim();
     }
 }
