6d7d3cd1eec0815d1726eff9c090c9d4368be1da
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-03-25 10:28:11 -0400

    finished fundamentals

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -4,7 +4,30 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
+
+    private VowelUtils vowelUtils;
+
     public String translate(String str) {
-        return null;
+        String[] words = str.split(" ");
+        String translated = "";
+        for (String word : words) {
+            translated += translateOneWord(word) + " ";
+        }
+        return translated.trim();
     }
+
+    public String translateOneWord(String str) {
+        if (vowelUtils.startsWithVowel(str)) {
+            return str + "way";
+        } else if (!vowelUtils.hasVowels(str)) {
+            return str + "ay";
+        } else {
+            Integer indexOfVowel = vowelUtils.getIndexOfFirstVowel(str);
+            String substr1 = str.substring(0, indexOfVowel);
+            String substr2 = str.substring(indexOfVowel);
+            return substr2 + substr1 + "ay";
+        }
+    }
+
+
 }
